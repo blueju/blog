7d785e3bf79c242dbea5617e7c9db1ea1ea28870
LanJu <lan-ju@qq.com>
2019-05-09 21:22:17 +0800

    改造完成

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 var CONFIG = {
   // 网页 title
-  title: 'Book-Cli',
+  title: 'blueju-blog',
   
   // 网页 keywords
-  keywords: 'book, book-cli, doc, example',
+  keywords: 'blueju, blog',
 
   // 网页 description
-  description: 'Init html book with markdown files.',
+  description: 'blueju\'s blog.',
 
   // 链接是否打开新的窗口
   openNewWindow: true,
 
   // 页面右上角的 github 链接
-  github: 'https://github.com/vvpvvp/book-cli',
+  github: 'https://github.com/blueju/blog',
 
   // 左侧目录的md文件
   summaryMd: 'SUMMARY.md',
---
+++ docs/BLOG.md
@@ -0,0 +1,13 @@
+# 介绍
+
+blog.blueju.cn 是一个用于记录我博客并跳转的中转平台。
+
+# 文章
+
+## 谁在使用book-cli？
+
+* [ECMAScript 6](https://es6.ch-un.com/)
+* [HeyUI Admin Docs](https://heyui.github.io/heyui-admin-docs/)
+* [前端基础文档](https://llbteam.github.io/docs.fe/)
+
+
